2d7db0b7cd4297d0a0f0bb812567eec899e4c406
Jacob Wilkins <jacobwilkins501@gmail.com>
2019-10-20 17:40:14 -0500

    update BM25 algorithm to match one from Wikipedia

--- movieclassifier.py
+++ movieclassifier.py
@@ -13,11 +13,12 @@ import json
 import math
 import tempfile
 import hashlib
+import shutil
 
 __version__ = (1, 0, 0)
 
 class Movieclassifier(object):
-    
+
     stopwords = set([
         'a', 'an', 'and', 'are', 'as', 'at', 'be', 'but', 'by',
         'for', 'if', 'in', 'into', 'is', 'it',
@@ -25,17 +26,17 @@ class Movieclassifier(object):
         't', 'that', 'the', 'their', 'then', 'there', 'these',
         'they', 'this', 'to', 'was', 'will', 'with'
     ])
-    
+
     punctuation = re.compile('[~`!@#$%^&*()+={\[}\]|\\:;"\',<.>/?]')
-    
+
     def __init__(self, base_directory):
-        
+
         self.base_directory = base_directory
         self.index_path = os.path.join(self.base_directory, 'index')
         self.docs_path = os.path.join(self.base_directory, 'documents')
         self.stats_path = os.path.join(self.base_directory, 'stats.json')
         self.setup()
-    
+
     def setup(self):
         if not os.path.exists(self.base_directory):
             os.makedirs(self.base_directory)
@@ -47,7 +48,7 @@ class Movieclassifier(object):
             os.makedirs(self.docs_path)
 
         return True
-    
+
     def read_stats(self):
         if not os.path.exists(self.stats_path):
             return {
@@ -57,43 +58,43 @@ class Movieclassifier(object):
 
         with open(self.stats_path, 'r') as stats_file:
             return json.load(stats_file)
-        
+
     def write_stats(self, new_stats):
         with open(self.stats_path, 'w') as stats_file:
             json.dump(new_stats, stats_file)
 
         return True
-    
+
     def increment_total_docs(self):
         current_stats = self.read_stats()
         current_stats.setdefault('total_docs', 0)
         current_stats['total_docs'] += 1
         self.write_stats(current_stats)
-        
+
     def get_total_docs(self):
         current_stats = self.read_stats()
         return int(current_stats.get('total_docs', 0))
-    
+
     def get_tokens(self, blob):
         blob = self.punctuation.sub(' ', blob) #strip the punctuation
         tokens = []
-        
+
         for token in blob.split():
-            
+
             token = token.lower().strip()
 
             if not token in self.stopwords:
                 tokens.append(token)
 
         return tokens
-    
+
     def make_tokens(self, blob):
         blob = self.punctuation.sub(' ', blob) #strip the punctuation
         tokens = []
         ps = PorterStemmer()
-        
+
         for token in blob.split():
-            
+
             token = token.lower().strip() #strip whitespace
             token = ps.stem(token) #stem the token
 
@@ -101,13 +102,13 @@ class Movieclassifier(object):
                 tokens.append(token)
 
         return tokens
-    
+
     def make_ngrams(self, tokens, min_gram = 3, max_gram = 6):
         terms = {}
-        
+
         for position, token in enumerate(tokens):
             for window_length in range(min_gram, min(max_gram + 1, len(token) + 1)):
-                
+
                 gram = token[:window_length]
                 terms.setdefault(gram, [])
 
@@ -115,22 +116,22 @@ class Movieclassifier(object):
                     terms[gram].append(position)
 
         return terms
-    
+
     def hash_name(self, term, length = 6):
         term = term.encode('ascii', errors='ignore')
-        
+
         hashed = hashlib.md5(term).hexdigest()
         return hashed[:length]
-    
+
     def make_segment_name(self, term):
         return os.path.join(self.index_path, "{0}.index".format(self.hash_name(term)))
-    
+
     def parse_record(self, line):
         return line.rstrip().split('\t', 1)
-    
+
     def make_record(self, term, term_info):
         return "{0}\t{1}\n".format(term, json.dumps(term_info, ensure_ascii=False))
-    
+
     def update_term_info(self, orig_info, new_info):
         for doc_id, positions in new_info.items():
             if not doc_id in orig_info:
@@ -142,12 +143,12 @@ class Movieclassifier(object):
                 orig_info[doc_id] = list(orig_positions)
 
         return orig_info
-    
+
     def save_segment(self, term, term_info, update=False):
         seg_name = self.make_segment_name(term)
         new_seg_file = tempfile.NamedTemporaryFile(delete=False)
         written = False
-        
+
         if not os.path.exists(seg_name):
             with open(seg_name, 'w') as seg_file:
                 seg_file.write('')
@@ -177,12 +178,13 @@ class Movieclassifier(object):
 
         new_seg_file.close()
         try:
-            os.rename(new_seg_file.name, seg_name)
+            shutil.move(new_seg_file.name, seg_name)
+            #os.rename(new_seg_file.name, seg_name)
         except OSError:
             os.remove(seg_name)
             os.rename(new_seg_file.name, seg_name)
         return True
-    
+
     def load_segment(self, term):
         seg_name = self.make_segment_name(term)
 
@@ -197,10 +199,10 @@ class Movieclassifier(object):
                     return json.loads(term_info)
 
         return {}
-    
+
     def make_document_name(self, doc_id):
         return os.path.join(self.docs_path, self.hash_name(doc_id), "{0}.json".format(doc_id))
-    
+
     def save_document(self, doc_id, document):
         doc_path = self.make_document_name(doc_id)
         base_path = os.path.dirname(doc_path)
@@ -212,7 +214,7 @@ class Movieclassifier(object):
             doc_file.write(json.dumps(document, ensure_ascii=False))
 
         return True
-    
+
     def load_document(self, doc_id):
         doc_path = self.make_document_name(doc_id)
 
@@ -220,7 +222,7 @@ class Movieclassifier(object):
             data = json.loads(doc_file.read())
 
         return data
-    
+
     def index(self, doc_id, document):
         if not hasattr(document, 'items'):
             raise AttributeError('You must provide `index` with a document in the form of a dictionary.')
@@ -230,7 +232,7 @@ class Movieclassifier(object):
 
         doc_id = str(doc_id)
         self.save_document(doc_id, document)
-        
+
         tokens = self.make_tokens(document.get('text', ''))
         terms = self.make_ngrams(tokens)
 
@@ -239,11 +241,11 @@ class Movieclassifier(object):
 
         self.increment_total_docs()
         return True
-    
+
     def parse_query(self, query):
         tokens = self.make_tokens(query)
         return self.make_ngrams(tokens)
-    
+
     def collect_results(self, terms):
         per_term_docs = {}
         per_doc_counts = {}
@@ -260,18 +262,16 @@ class Movieclassifier(object):
                 per_doc_counts[doc_id][term] += len(positions)
 
         return per_term_docs, per_doc_counts
-    
-    def bm25_relevance(self, terms, matches, current_doc, total_docs, b, k):
-        score = b
+
+    def bm25_relevance(self, terms, matches, current_doc, total_docs, curr_len, avg_len, b, k):
+        score = 0.0
 
         for term in terms:
-            if matches[term] == 0.0:
-                continue
-            idf = math.log((total_docs - matches[term] + 1.0) / matches[term]) / math.log(1.0 + total_docs)
-            score = score + current_doc.get(term, 0) * idf / (current_doc.get(term, 0) + k)
+            idf = math.log((total_docs - matches[term] + 0.5) / (matches[term] + 0.5))
+            score = score + (current_doc.get(term, 0) * idf * (k + 1)) / (current_doc.get(term, 0) + k * (1 - b + (b * (curr_len / avg_len))))
 
-        return 0.5 + score / (2 * len(terms))
-    
+        return score
+
     def search(self, query, offset = 0, limit = 20):
         results = {
             'total_hits': 0,
@@ -290,10 +290,15 @@ class Movieclassifier(object):
         per_term_docs, per_doc_counts = self.collect_results(terms)
         scored_results = []
 
+        total_length = 0.0
+        for doc_id, current_doc in per_doc_counts.items():
+            total_length += len(current_doc)
+        avg_length = total_length / total_docs
+
         for doc_id, current_doc in per_doc_counts.items():
             scored_results.append({
                 'id': doc_id,
-                'score': self.bm25_relevance(terms, per_term_docs, current_doc, total_docs, len(current_doc), 1.2),
+                'score': self.bm25_relevance(terms, per_term_docs, current_doc, total_docs, len(current_doc), avg_length, 0.75, 1.2),
             })
 
         sorted_results = sorted(scored_results, key=lambda res: res['score'], reverse=True)
