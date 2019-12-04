2e51a351bfe82c26b6f8dc96c3572126c9d772bf
Jacob Wilkins <jacobwilkins501@gmail.com>
2019-12-04 10:28:47 -0600

    Update textclassify.py

--- textclassify.py
+++ textclassify.py
@@ -1,75 +1,58 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Mon Nov  4 17:32:00 2019
+Created on Sun Dec  1 21:27:21 2019
 
 @author: jacobwilkins
 """
-'''
-import sklearn
-from sklearn.naive_bayes import BernoulliNB
-from sklearn.naive_bayes import GaussianNB
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.cross_validation import train_test_split
 
-from sklearn import metrics
-'''
+import re, pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.linear_model import LogisticRegression
-from nltk.stem import PorterStemmer
-import re
+from sklearn.ensemble import RandomForestClassifier
+from nltk.stem import WordNetLemmatizer
 
 class Textclassify(object):
     
-    genres = ["Action", "Adventure", "Animation", "Comedy", "Crime", "Documentory",
-              "Drama", "Family", "Fantasy", "Foreign", "History", "Horror", "Music",
-              "Mystery", "Science Fiction", "Thriller", "TV Movie", "War", "Western"]
+    def __init__(self, trainData): self.trainData = trainData
     
-    stopwords = set([
-        'a', 'an', 'and', 'are', 'as', 'at', 'be', 'but', 'by',
-        'for', 'if', 'in', 'into', 'is', 'it',
-        'no', 'not', 'of', 'on', 'or', 's', 'such',
-        't', 'that', 'the', 'their', 'then', 'there', 'these',
-        'they', 'this', 'to', 'was', 'will', 'with'
-    ])
+    def preprocess(self, x):
+        docs = []; stemmer = WordNetLemmatizer()
+        for k in range(0, len(x)):
+            doc = re.sub(r'\W', ' ', str(x[k])) # Remove all the special characters
+            doc = re.sub(r'\s+[a-zA-Z]\s+', ' ', doc) # remove all single characters
+            doc = re.sub(r'\^[a-zA-Z]\s+', ' ', doc) # Remove single characters from the start
+            doc = re.sub(r'\s+', ' ', doc, flags=re.I) # Substituting multiple spaces with single space
+            doc = re.sub(r'^b\s+', '', doc) # Removing prefixed 'b'
+            doc = doc.lower() # Converting to Lowercase
+            doc = doc.split() # Lemmatization
+            doc = [stemmer.lemmatize(word) for word in doc]
+            doc = ' '.join(doc); docs.append(doc)
+        return docs
     
-    punctuation = re.compile('[~`!@#$%^&*()+={\[}\]|\\:;"\',<.>/?]')
-    
-    def __init__(self, trainData):
-        self.trainData = trainData
-    
-    def cleanDescr(self, descr):
-        descr = self.punctuation.sub(' ', descr)
-        ps = PorterStemmer()
+    def classify(self, testData):
+        x_test = [testData]; x_train = []; y_train = []
+        for d in self.trainData:
+            for key in d:
+                if key == 'text': x_train.append(d[key])
+                if key == 'genres':
+                    if d[key] == []: y_train.append('other')
+                    else:
+                        for i in d[key]: y_train.append(i['name']); break
+                        
+        x_train = self.preprocess(x_train); x_test = self.preprocess(x_test)
         
-        for token in descr.split():
-            
-            token = token.lower().strip() #strip whitespace
-            token = ps.stem(token) #stem the token
+        trainData = pd.DataFrame({'plot': x_train, 'tags': y_train})
+        testData = pd.DataFrame({'plot': x_test, 'tags': ['Comedy']})
 
-            if token in self.stopwords:
-                descr = self.token.sub(' ', descr)
-        return descr
-    
-    def createFeatVect(self, trainData, genre):
-        descriptions = []
-        for movie in trainData.items():
-            if (movie.get['genre'] == genre): descriptions.append(self.cleanDescr(movie.get['text']))
-        vectorizer = CountVectorizer(min_df = 0, lowercase = True)
-        vectorizer.fit(descriptions)
-        vectorizer.transform(descriptions).toarray()
-        return descriptions
-    
-    def classify(self, query):
-        results = []
+        vectorizer = CountVectorizer(analyzer="word", tokenizer=None, preprocessor=None, stop_words=None, max_features=3000)
+        train_data_features = vectorizer.fit_transform(trainData['plot'])
+        train_data_features = train_data_features.toarray()
+
+        classifier = RandomForestClassifier(n_estimators=1000, random_state=0)
+        classifier = classifier.fit(train_data_features, trainData['tags'])
         
-        query = self.cleanDescr(query)
-        vectorizer = CountVectorizer(min_df = 0, lowercase = True)
-        vectorizer.fit(query)
-        vectorizer.transform(query).toarray()
+        test_data_features = vectorizer.transform(testData['plot'])
+        test_data_features = test_data_features.toarray()
+        pred = classifier.predict(test_data_features)
         
-        for genre in self.genres:
-            classifier = LogisticRegression()
-            featVect = self.createFeatVect(self.trainData, genre)
-               
-        return
+        return pred
